5a72c13791c17fc6ff6f82e87517a08a471c2701
Logan Underwood <lunderwood@vailsys.com>
2023-12-06 01:07:04 -0600

    Add body

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
 import React from 'react'
 import './res/scss/styles.scss'
 import { Route, Routes } from 'react-router-dom'
-import { SideNav } from './components/SideNav/SideNav'
-import { Title } from './components/Title/Title'
 import { routes } from './routes'
 import { type ReactElement } from 'react'
+import { SideNav } from './components/SideNav/SideNav'
+import { Title } from './components/Title/Title'
+import { GettingStarted } from './pages/GettingStarted/GettingStarted'
 
 function App (): ReactElement {
   return (
     <div className="app-container">
-      <div className="app-container__content">
+      <div className="app-container__side">
         <Title />
         <SideNav />
       </div>
-      <Routes>
-        <Route path={routes.root} element={ <>Root</> } />
-        <Route path={routes.gettingStarted} element={ <>Getting Started</>} />
-        <Route path={routes.hardware.audioInterfaces} element={ <>Audio Interfaces</>} />
-        <Route path={routes.hardware.microphones} element={ <>Microphones</>} />
-        <Route path={routes.software.DAWs} element={ <>DAWs</>} />
-        <Route path={routes.software.plugins} element={ <>Plugins</>} />
-        <Route path={routes.technique.mixing} element={ <>Mixing</>} />
-        <Route path={routes.technique.mastering} element={ <>Mastering</>} />
-        <Route path={routes.advancedResources} element={ <>Advanced Resources</>} />
-      </Routes>
+      <div className="app-container__content">
+        <Routes>
+          <Route path={routes.root} element={ <>Root</> } />
+          <Route path={routes.gettingStarted} element={ <GettingStarted />} />
+          <Route path={routes.hardware.audioInterfaces} element={ <>Audio Interfaces</>} />
+          <Route path={routes.hardware.microphones} element={ <>Microphones</>} />
+          <Route path={routes.software.DAWs} element={ <>DAWs</>} />
+          <Route path={routes.software.plugins} element={ <>Plugins</>} />
+          <Route path={routes.technique.mixing} element={ <>Mixing</>} />
+          <Route path={routes.technique.mastering} element={ <>Mastering</>} />
+          <Route path={routes.advancedResources} element={ <>Advanced Resources</>} />
+        </Routes>
+      </div>
     </div>
   )
 }
---
+++ src/pages/GettingStarted/GettingStarted.tsx
@@ -0,0 +1,11 @@
+import { type FunctionComponent } from 'react'
+
+export const GettingStarted: FunctionComponent = () => {
+  return (
+    <div className='getting-started'>
+      <header className='getting-started__title'>
+        Getting Started
+      </header>
+    </div>
+  )
+}
